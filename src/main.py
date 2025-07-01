14cdc3c33c89200f0b7b6cbcb6cef6681bc2d660
fernando-peres <fernando.peres@icloud.com>
2025-07-01 17:55:48 +0100

    changes: main add/del issues

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 """
-Main file - basic code to test ruff and mypy
+Main file - basic code to test ruff and mypy.
+
+if x then:
+call_func()
 """
 
 
@@ -12,18 +15,14 @@ MY_LIST = [
     "John",
     "Joe",
     "Jill",
-    "Jack",
-    "Jill",
-    "Jane",
-    "John",
-    "Joe",
-    "Jill",
-    "Jack",
 ]
 
 MY_DICT = {"key": "value", "key2": "value2", "key3": "value3"}
 
-long_string = "This is a very long line that exceeds the maximum allowed  line length for \
-    Python code. This is a very long line that exceeds the maximum allowed"
+long_string = (
+    "This is a very long line that exceeds the maximum allowed line length for \
+    Python code. This is a very long line that exceeds the \
+    maximum allowed"
+)
 
 MY_TUPLE = (1, 2, 3)
