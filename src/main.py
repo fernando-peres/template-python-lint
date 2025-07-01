8eee7cc0e8cc10b6d8f0518bdde7a4898b5ea02c
fernando-peres <fernando.peres@icloud.com>
2025-07-01 11:38:26 +0100

    added: pre-commit hooks

--- src/main.py
+++ src/main.py
@@ -12,10 +12,6 @@ def main() -> None:
 
     print("Hello World!")
 
-    print("a")
-    print("b")
-    print("c")
-
 
 def foo() -> None:
     print("foo")
@@ -35,14 +31,10 @@ MY_LIST = [
     "Jack",
 ]
 
-MY_DICT = {
-    "key": "value",
-    "key2": "value2",
-    "key3": "value3",
-}
+MY_DICT = {"key": "value", "key2": "value2", "key3": "value3"}
 
 long_string = "This is a very long line that exceeds the maximum allowed  line length for \
-Python code. This is a very long line that exceeds the maximum allowed"
+    Python code. This is a very long line that exceeds the maximum allowed"
 
 MY_TUPLE = (1, 2, 3)
 
